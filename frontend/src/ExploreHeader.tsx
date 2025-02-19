beabbaa750a1dd9f90da7977d6e90a1ac60b54d5
Nik Nordquist <nnordquist3@gmail.com>
2025-02-19 11:46:19 -0600

    Created explore header component

---
+++ frontend/src/ExploreHeader.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ExploreHeader() {
+    return(
+        <header>
+            <h1 style={{color: 'white'}}>Explore Header Test Text</h1>
+        </header>
+    );
+}
+
+export default ExploreHeader;
