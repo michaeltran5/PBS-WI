84391aa4fdafc64929b2bfa06248549f979f618d
Owen Loucks <owenloucks@gmail.com>
2025-02-20 00:17:27 -0600

    created media player page and browse page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,21 @@
 import './App.css'
 import Header from './components/header'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Browse from './pages/Browse'
+import MediaPlayer from './pages/MediaPlayer'
 
 function App() {
   return (
-    <div className="app-container">
-      <Header />
-    </div>
+    <Router>
+      <div className="app-container">
+        <Header />
+        <Routes>
+        <Route path="/" element={<Browse />} />
+          <Route path="/browse" element={<Browse />} />
+          <Route path="/watch" element={<MediaPlayer />} />
+        </Routes>
+      </div>
+    </Router>
   )
 }
 
--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -1,9 +1,9 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import Image from 'react-bootstrap/Image';
 import { Search, User } from 'lucide-react';
+import { Link } from 'react-router-dom';
 
 function Header() {
     return (
@@ -22,7 +22,7 @@ function Header() {
                 </Navbar.Brand>
                 <Nav className="me-auto">
                     {/* This holds all the different redirection links, will have to redo with actual link names */}
-                    <Nav.Link href="#browse" className="text-white me-4">Browse</Nav.Link>
+                    <Nav.Link as={Link} to="/" className="text-white fw-bold me-4">Browse</Nav.Link>
                     <Nav.Link href="#live" className="text-white me-4">Live</Nav.Link>
                     <Nav.Link href="#mylist" className="text-white me-4">My List</Nav.Link>
                     <Nav.Link href="#genre" className="text-white me-4">Browse by Genre</Nav.Link>
