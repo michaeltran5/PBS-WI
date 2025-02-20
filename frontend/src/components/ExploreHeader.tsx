ceb09c99ec3d51c9a2d75688182fb54bd5507d37
Nik Nordquist <nnordquist3@gmail.com>
2025-02-20 08:16:56 -0600

    Added navbar page links, logo

--- frontend/src/components/ExploreHeader.tsx
+++ frontend/src/components/ExploreHeader.tsx
@@ -1,14 +1,26 @@
 import React from 'react';
-import {Navbar, Container} from 'react-bootstrap';
+import {Navbar, Nav, Container} from 'react-bootstrap';
+import logo from '../assets/pbs_logo.png'
+import '../styled/ExploreHeader.css'
 
 function ExploreHeader() {
     return(
         <div>
             <Navbar bg="dark" variant="dark">
-                <Container>
+                <Container style={{display: 'flex', alignItems: "center"}}>
                     <Navbar.Brand>
-                    {' '}PBS Wisconsin
+                        <img
+                            src={logo}
+                            width={100}
+                            style={{marginTop: 20, marginLeft: 30}}
+                            />
                     </Navbar.Brand>
+                    <Nav style={{paddingTop: 20}}>
+                        <Nav.Link className="navbar-pages current-page">Browse</Nav.Link>
+                        <Nav.Link className="navbar-pages">Live</Nav.Link>
+                        <Nav.Link className="navbar-pages">My List</Nav.Link>
+                        <Nav.Link className="navbar-pages">Browse by Genre</Nav.Link>
+                    </Nav>
                 </Container>
             </Navbar>
         </div>
