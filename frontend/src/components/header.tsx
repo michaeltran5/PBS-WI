f1ec200d58687a00358f85ee1f26df075213cb55
michaeltran5 <mttran7@wisc.edu>
2025-02-24 12:46:53 -0600

    adding header

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -22,7 +22,11 @@ function Header() {
                 </Navbar.Brand>
                 <Nav className="me-auto">
                     {/* This holds all the different redirection links, will have to redo with actual link names */}
+<<<<<<< HEAD
                     <Nav.Link as={Link} to="/" className="text-white fw-bold me-4">Browse</Nav.Link>
+=======
+                    <Nav.Link href="#browse" className="text-white me-4">Browse</Nav.Link>
+>>>>>>> 820cd45 (header done)
                     <Nav.Link href="#live" className="text-white me-4">Live</Nav.Link>
                     <Nav.Link href="#mylist" className="text-white me-4">My List</Nav.Link>
                     <Nav.Link href="#genre" className="text-white me-4">Browse by Genre</Nav.Link>
