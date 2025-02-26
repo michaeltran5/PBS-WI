75cfa30612f8b6f6c2ac68597f88f8bd044e1ba9
michaeltran5 <mttran7@wisc.edu>
2025-02-26 16:22:20 -0600

    updated styling for the header in .styled.ts file

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css'
 
-import Header from './components/header'
+import Header from './components/Header'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Browse from './pages/Browse'
 import MediaPlayer from './pages/MediaPlayer'
